3709aa8f3ee1a91b0e8fc12dc41e7ff448ee6545
grjk <jkristiansen@mail.greenriver.edu>
2022-05-26 16:20:17 -0700

    Added initial Card component

--- json-todo-list/src/App.js
+++ json-todo-list/src/App.js
@@ -1,9 +1,13 @@
+import Card from "./components/Card.js";
 import "./css/styles.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">To-Do App</header>
+      <div>
+        <Card />
+      </div>
     </div>
   );
 }
