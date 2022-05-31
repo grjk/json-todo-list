cfb6a7f702330ddf17b66329ca7905b06c946dd9
Joshua Kristiansen <jykristiansen@gmail.com>
2022-05-31 16:34:07 -0700

    Added pagination, CRUD, and search functionality.

--- json-todo-list/src/App.js
+++ json-todo-list/src/App.js
@@ -4,32 +4,126 @@ import "./css/styles.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [currentDisplay, setCD] = useState([]);
-  const [limit, setLimit] = useState(10);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [apiHasMore, setAHM] = useState(true);
+  const [todoTitle, setTodoTitle] = useState("");
+  const [searchTerm, setST] = useState("");
+  const test = "hi";
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=" + limit)
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    searchTodos();
+  }, [searchTerm]);
+
+  const loadTodos = () => {
+    if (!apiHasMore) return;
+
+    fetch(
+      `https://jsonplaceholder.typicode.com/todos?_start=${todos.length}&_limit=10`
+    )
       .then((response) => response.json())
-      .then((json) => (console.log(json), setTodos(json)));
-  }, [limit]);
+      .then((json) => {
+        setTodos([...todos, ...json]);
+        if (json.length < 10) {
+          setAHM(false);
+        }
+      });
+  };
 
-  // const listInnerRef = useRef();
+  const createTodo = () => {
+    test = "bye";
+    if (!todoTitle) return;
+    fetch(`https://jsonplaceholder.typicode.com/todos`, {
+      method: "POST",
+      body: JSON.stringify({
+        completed: false,
+        title: todoTitle,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((createdTodo) => setTodos([createdTodo, ...todos]));
+  };
 
-  // const scrollCheck = () => {
-  //   if (listInnerRef.current) {
-  //     const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
-  //     if (scrollTop + clientHeight === scrollHeight) {
-  //       setLimit(limit + 10);
-  //     }
-  //   }
-  // };
+  const editTodo = ({ completed, id, title, userId }) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        completed: !completed,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((editedTodo) => {
+        const editedTodos = todos.map((todo) => {
+          if (todo.id === editedTodo.id) {
+            return editedTodo;
+          }
+
+          return todo;
+        });
+
+        setTodos(editedTodos);
+      });
+  };
+
+  const removeTodo = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "DELETE",
+    }).then(() => setTodos(todos.filter((todo) => todo.id != id)));
+  };
+
+  const searchTodos = () => {
+    const filtered = todos.filter((todo) => todo.title.includes(searchTerm));
+    setFilteredTodos(filtered);
+  };
+
+  const handleScroll = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) {
+      loadTodos();
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">To-Do App</header>
-      <div className="cards" onScroll={scrollCheck} /*ref={listInnerRef}*/>
-        {todos.map((item) => (
-          <Card data={item} />
+      <h1>{test}</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setST(e.target.value)}
+          value={searchTerm}
+        ></input>
+      </div>
+      <div className="create-todo">
+        Add new todo:
+        <input
+          type="text"
+          placeholder="Add title"
+          onChange={(e) => setTodoTitle(e.target.value)}
+          value={todoTitle}
+        ></input>
+        <button onClick={createTodo}>+</button>
+      </div>
+      <div className="card-container" onScroll={handleScroll}>
+        {(searchTerm ? filteredTodos : todos).map((item) => (
+          <Card
+            key={item.id}
+            data={item}
+            editTodo={editTodo}
+            removeTodo={removeTodo}
+          />
         ))}
       </div>
     </div>
--- json-todo-list/src/components/Card.js
+++ json-todo-list/src/components/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Card({ data }) {
+function Card({ data, editTodo, removeTodo }) {
   return (
     <div className="card">
       <div className="card-body">
-        <p className="card-title">{data.title}</p>
-        <p className="card-body">Completion: {data.completed ? "Yes" : "No"}</p>
+        <div className="card-title">{data.title}</div>
+        <div>Completion: {data.completed ? "Yes" : "No"}</div>
+        <button onClick={() => editTodo(data)}>Edit</button>
+        <button onClick={() => removeTodo(data.id)}>Delete</button>
       </div>
     </div>
   );
