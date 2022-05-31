e415157e7d054d0638e2e26c41c69424e34c764d
grjk <jkristiansen@mail.greenriver.edu>
2022-05-31 13:19:24 -0700

    Added incomplete changes for pagination

--- json-todo-list/src/App.js
+++ json-todo-list/src/App.js
@@ -1,12 +1,36 @@
+import { useState, useEffect, useRef } from "react";
 import Card from "./components/Card.js";
 import "./css/styles.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [currentDisplay, setCD] = useState([]);
+  const [limit, setLimit] = useState(10);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=" + limit)
+      .then((response) => response.json())
+      .then((json) => (console.log(json), setTodos(json)));
+  }, [limit]);
+
+  // const listInnerRef = useRef();
+
+  // const scrollCheck = () => {
+  //   if (listInnerRef.current) {
+  //     const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+  //     if (scrollTop + clientHeight === scrollHeight) {
+  //       setLimit(limit + 10);
+  //     }
+  //   }
+  // };
+
   return (
     <div className="App">
       <header className="App-header">To-Do App</header>
-      <div>
-        <Card />
+      <div className="cards" onScroll={scrollCheck} /*ref={listInnerRef}*/>
+        {todos.map((item) => (
+          <Card data={item} />
+        ))}
       </div>
     </div>
   );
--- json-todo-list/src/components/Card.js
+++ json-todo-list/src/components/Card.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Card() {
-  return <div>Card</div>;
+function Card({ data }) {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <p className="card-title">{data.title}</p>
+        <p className="card-body">Completion: {data.completed ? "Yes" : "No"}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
